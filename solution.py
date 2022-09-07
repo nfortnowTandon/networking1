82e0c27c0c1c5293403a9220fcffda9dabdbe2ee
nfortnowTandon <mnf7261@nyu.edu>
2022-09-07 12:09:35 -0500

    commenting out my tests

--- solution.py
+++ solution.py
@@ -31,13 +31,13 @@ def welcome_assignment_answers(question):
 
 
 # noinspection PyUnreachableCode
-debug_questions = ["In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?",
-                   "Are encoding and encryption the same? - Yes/No","Is it possible to decrypt a message without a key? - Yes/No",
-                   "Is it possible to decode a message without a key? - Yes/No",
-                   "Is a hashed message supposed to be un-hashed? - Yes/No",
-                   "What is the SHA1 hashing value to the following message: 'NYU Computer Networking' - Use SHA1 hash generator and use the answer in your code",
-                   "Is MD5 a secured hashing algorithm? - Yes/No",
-                   "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number",
-                   "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number"]
-for q in debug_questions:
-    print(welcome_assignment_answers(q))
+# debug_questions = ["In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?",
+#                    "Are encoding and encryption the same? - Yes/No","Is it possible to decrypt a message without a key? - Yes/No",
+#                    "Is it possible to decode a message without a key? - Yes/No",
+#                    "Is a hashed message supposed to be un-hashed? - Yes/No",
+#                    "What is the SHA1 hashing value to the following message: 'NYU Computer Networking' - Use SHA1 hash generator and use the answer in your code",
+#                    "Is MD5 a secured hashing algorithm? - Yes/No",
+#                    "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number",
+#                    "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number"]
+# for q in debug_questions:
+#     print(welcome_assignment_answers(q))
